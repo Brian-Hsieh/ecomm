160f802843f84e1c69285303557dff6f615a59dd
brian <brianhsieh38@gmail.com>
2024-07-28 16:31:47 +1000

    Added sql database connection
    - configuation missing

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,13 @@ import (
 	"log"
 
 	"github.com/Brian-Hsieh/ecomm/cmd/ecomm"
+	"github.com/Brian-Hsieh/ecomm/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	db, err := db.NewSQLDatabase(mysql.Config{})
+
 	server := ecomm.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatalln("Unable to run the API server")
