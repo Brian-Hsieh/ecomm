3777c4465f1b6b1748e2d2caba8240a10fc25455
brian <brianhsieh38@gmail.com>
2024-07-31 20:45:22 +1000

    Added user type for in-memory store

--- pkg/types.go
+++ pkg/types.go
@@ -1,7 +1,18 @@
 package pkg
 
+import "time"
+
 type UserPayload struct {
 	Name     string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
+}
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"username"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	DOB       string    `json:"birthDate"`
+	CreatedAt time.Time `json:"createdAt"`
 }
