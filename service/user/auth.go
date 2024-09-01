f046675ef77002399993879a6ad063e9395a6126
brian <brianhsieh38@gmail.com>
2024-09-01 21:32:22 +1000

    Created function to generate jwt token

--- service/user/auth.go
+++ service/user/auth.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+	"time"
+
+	"github.com/Brian-Hsieh/ecomm/config"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,4 +14,18 @@ func hashPassword(password string) ([]byte, error) {
 		return nil, err
 	}
 	return pwd, nil
+}
+
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"exp":      time.Now().Add(time.Minute * 15).Unix(),
+	})
+
+	tokenString, err := token.SignedString(config.Env.Secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
